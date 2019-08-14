02edb392a66e38d788bacc9a32e45c904321629d
Stewart ilondanga <stilondanga@gmail.com>
2019-08-14 13:27:50 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-/*$(document).ready(function(){
-    $('input[type="checkbox"]').click(function(){
-        if($(this).prop("checked") == true){
-          $checkVal = $(this).val();
-          $('#doneList').append('<li><input type="checkbox" name="'+ $checkVal +'" value="'+ $checkVal +'" checked><label for="'+ $checkVal +'">' + $checkVal +'</label></li>');
-          $(this).siblings().remove();
-          $(this).remove();
-        } else if($(this).prop("checked") == false){
-          alert('not checked');
-          $(this).siblings().remove();
-          $(this).remove();
-        }
-    });
+$(document).ready(function() {
+  $('input[type="checkbox"]').click(function() {
+    if ($(this).prop("checked") == true) {
+      $checkVal = $(this).val();
+      $('#doneList').append('<li><input type="checkbox" name="' + $checkVal + '" value="' + $checkVal + '" checked><label for="' + $checkVal + '">' + $checkVal + '</label></li>');
+      $(this).siblings().remove();
+      $(this).remove();
+    } else if ($(this).prop("checked") == false) {
+      alert('not checked');
+      $(this).siblings().remove();
+      $(this).remove();
+    }
+  });
 
-   // $('#doneList input[type="checkbox"]').click(function(){
-   //      alert("checked has been unchecked");
-   //  });
+  // $('#doneList input[type="checkbox"]').click(function(){
+  //      alert("checked has been unchecked");
+  //  });
 });
 
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
