e984f48770d0197266dd3ce67904782daf67420e
Stewart ilondanga <stilondanga@gmail.com>
2019-08-14 13:27:17 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+/*$(document).ready(function(){
+    $('input[type="checkbox"]').click(function(){
+        if($(this).prop("checked") == true){
+          $checkVal = $(this).val();
+          $('#doneList').append('<li><input type="checkbox" name="'+ $checkVal +'" value="'+ $checkVal +'" checked><label for="'+ $checkVal +'">' + $checkVal +'</label></li>');
+          $(this).siblings().remove();
+          $(this).remove();
+        } else if($(this).prop("checked") == false){
+          alert('not checked');
+          $(this).siblings().remove();
+          $(this).remove();
+        }
+    });
+
+   // $('#doneList input[type="checkbox"]').click(function(){
+   //      alert("checked has been unchecked");
+   //  });
+});
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
